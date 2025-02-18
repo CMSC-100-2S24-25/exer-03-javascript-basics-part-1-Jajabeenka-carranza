ef56195ac01c0a6d676e3a3f5e63005adbf074f1
Jajabeenka-carranza <jacarranza@up.edu.ph>
2025-02-18 14:07:51 +0800

    feat: added reversePassword and storePassword functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function validatePassword(x, y) {
-    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     //check if x and y are string
     if (typeof(x) == "string" && typeof(y) == "string") {
@@ -15,42 +15,73 @@ function validatePassword(x, y) {
                     if (lowerCase.some(check => x.includes(check))) {
                         //check if it has uppercase
                         if (upperCase.some(check => x.includes(check))) {
-                            return true
+                            return true;
                         }
                         else {
-                            return false
+                            return false;
                         }
                     }
                     else {
-                        return false
+                        return false;
                     }
                 }
                 else {
-                    return false
+                    return false;
                 }
             }
             else {
-                return false
+                return false;
             }
         }
         else {
-            return false
+            return false;
         }
     }
     else {
-        return false
-    x, y} {
-
-}
+        return false;
+    } 
 }
 
-function reversePassword(x, y) {
-    if (typeof(x) == "string" && typeof(y) == "string") {
-
+function reversePassword(x) {
+    if (typeof(x) == "string") {
+        let orig = x.split("");
+        let reversed = [];
+        let i = orig.length - 1;
+        while (i >= 0) {
+            reversed.push(orig[i]);
+            i--;
+        }
+        //remove all ","
+        let result = reversed.toString().split(",").join("");
+        return result;
+    }
+    else {
+        return "Not a string";
     }
 }
 
-console.log(validatePassword("Xasdasdasdasdadadadasda1", "Xasdasdasdasdadadadasda1"))
+function storePassword(name, pass1, pass2) {
+    let newPass;
+    if (validatePassword(pass1, pass1)) {
+        newPass = reversePassword(pass1);;
+    }
+    else {
+        newPass = pass1;
+    }
+
+    const user = {
+        name: name,
+        pass: newPass
+    }
+
+    return user;
+}
+
+console.log(storePassword("John", "Pass1234", "Pass1234"));
+console.log(storePassword("John", "Pass123", "Pass12345"));
+
+// console.log(validatePassword("Xasdasdasdasdadadadasda1", "Xasdasdasdasdadadadasda1"))
+// console.log(reversePassword("String"))
 
 //References
 //https://stackoverflow.com/questions/37896484/multiple-conditions-for-javascript-includes-method
